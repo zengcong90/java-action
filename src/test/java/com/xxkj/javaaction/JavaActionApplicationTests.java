429ec17c5e4727177dcfccee41b5fa0089770cba
zengcong <772579123@qq.com>
2025-08-13 10:18:32 +0800

    第一次提交

---
+++ src/test/java/com/xxkj/javaaction/JavaActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxkj.javaaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
